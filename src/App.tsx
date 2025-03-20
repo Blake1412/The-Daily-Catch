6e081a0186a56858414314be8f042e3660a90244
Blake Ryan <blake@blakeryan30@gmail.com>
2025-03-20 13:38:00 +0000

    adding functionality and redux

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,8 @@ function App() {
             message: 'Hello world!',
             image: 'https://picsum.photos/250/300',
             likes: 10,
-            comments: 4,
+            userLiked: false,
+            comments: [{ text: "Nice", postedAt: new Date(),author: "Mike", authorProfilePicture: 'https://picsum.photos/260/300'}, { text: "Very cool", postedAt: new Date(),author: "Dave", authorProfilePicture: 'https://picsum.photos/260/303'},],
         },
         {
             authorName: 'John',
@@ -29,7 +30,8 @@ function App() {
             message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
             image: 'https://picsum.photos/260/300',
             likes: 10,
-            comments: 4,
+            userLiked: false,
+            comments: [],
         },
         {
             authorName: 'John',
@@ -41,7 +43,8 @@ function App() {
             message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
             image: 'https://picsum.photos/400/300',
             likes: 10,
-            comments: 4,
+            userLiked: false,
+            comments: [],
         },
         {
             authorName: 'John',
@@ -53,7 +56,8 @@ function App() {
             message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum             message: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
             image: 'https://picsum.photos/4000/4000',
             likes: 10,
-            comments: 4,
+            userLiked: false,
+            comments: [],
         }
     ]
 
--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,6 +1,9 @@
 import Icon from "@mdi/react";
-import {mdiHeartOutline, mdiCommentOutline, mdiShareOutline} from '@mdi/js';
+import { mdiHeartOutline, mdiCommentOutline, mdiShareOutline, mdiHeart, mdiComment } from '@mdi/js';
 import { formatDistanceToNow } from 'date-fns';
+import { commentProps } from "../Comment/Comment";
+import Comment from "../Comment/Comment";
+import { useState } from "react";
 
 export type PostProps = {
     authorName: string;
@@ -12,11 +15,25 @@ export type PostProps = {
     message: string;
     image: string;
     likes: number;
-    comments: number;
+    userLiked: boolean;
+    comments: commentProps[];
 }
 
-const Post = (postInfo : PostProps) => {
-    const {authorName, authorProfilePicture, createdAt, fishType, location, fishWeight, message, image, likes, comments} = postInfo;
+const Post = (postInfo: PostProps) => {
+    const { authorName, authorProfilePicture, createdAt, fishType, location, fishWeight, message, image, likes, userLiked, comments } = postInfo;
+
+    const [isCommentsOpen, setIsCommentsOpen] = useState(false);
+    const [likesCount, setLikesCount] = useState(likes)
+    const [isLiked, setIsLiked] = useState(userLiked)
+
+    const toggleComments = () => {
+        setIsCommentsOpen(!isCommentsOpen)
+    }
+
+    const toggleLiked = () => {
+        setLikesCount(isLiked ? likesCount - 1 : likesCount + 1)
+        setIsLiked(!isLiked)
+    }
 
     return <>
         <div className="bg-white shadow-lg rounded-2xl p-4 flex flex-col gap-4">
@@ -27,7 +44,7 @@ const Post = (postInfo : PostProps) => {
             </div>
 
             <div className="w-full h-100 bg-gray-200 rounded-lg flex items-center justify-center">
-                <img src={image} alt="Post" className="w-full h-full object-cover rounded-lg"/>
+                <img src={image} alt="Post" className="w-full h-full object-cover rounded-lg" />
             </div>
 
             <div className="flex justify-between items-center mt-3 text-gray-700 text-sm font-semibold">
@@ -38,19 +55,25 @@ const Post = (postInfo : PostProps) => {
             <p className="text-gray-600 text-sm mt-2 flex-grow text-ellipsis">{message}</p>
 
             <div className="flex justify-between items-center mt-3 text-gray-500 text-sm">
-                <button className="flex items-center space-x-1 hover:text-red-500">
-                    <span>Like {likes}</span>
-                    <Icon path={mdiHeartOutline} size={1}></Icon>
+                <button className="flex items-center space-x-1 hover:text-red-500 cursor-pointer" onClick={toggleLiked}>
+                    <span>Like {likesCount}</span>
+                    <Icon path={isLiked ? mdiHeart : mdiHeartOutline} color={'red'} size={1}></Icon>
                 </button>
-                <button className="flex items-center space-x-1 hover:text-blue-500">
-                    <span>Comment {comments}</span>
-                    <Icon path={mdiCommentOutline} size={1}></Icon>
+                <button className="flex items-center space-x-1 hover:text-blue-500 cursor-pointer" onClick={toggleComments}>
+                    <span>Comment {comments.length >= 1 ? comments.length : null}</span>
+                    <Icon path={isCommentsOpen ? mdiComment : mdiCommentOutline} color={'#42A5F5'} size={1}></Icon>
                 </button>
-                <button className="flex items-center space-x-1 hover:text-green-500">
+                <button className="flex items-center space-x-1 hover:text-green-500 cursor-pointer">
                     <span>Share</span>
-                    <Icon path={mdiShareOutline} size={1}></Icon>
+                    <Icon path={mdiShareOutline} color={'green'} size={1}></Icon>
                 </button>
-            </div>
+            </div >
+            {isCommentsOpen && <div className="flex flex-col w-full max-w-md gap-5">
+                {comments.map((commentInfo) => (
+                    <Comment {...commentInfo} ></Comment>
+                ))}
+            </div>}
+
         </div>
     </>
 }
